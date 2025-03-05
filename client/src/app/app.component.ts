9c3a65955878598b54a00623a785a0401aaf4854
pankajkeshari099 <85383471+pankajkeshari099@users.noreply.github.com>
2025-03-05 05:59:04 +0000

    lots of changes and addes dotnety code

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent,RouterOutlet,RouterLink],
+  imports: [HeaderComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
